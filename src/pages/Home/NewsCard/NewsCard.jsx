769812f1670b37dd40c30bfc09c5cc2b262be2e0
Joyantichy <120155482+Joyantichy@users.noreply.github.com>
2023-05-03 03:01:40 +0600

    raiting added

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 import { Card, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FaBeer, FaEye, FaRegBookmark, FaRegStar, FaShareAlt, FaStar } from 'react-icons/fa';
-import Rating from 'react-rating';
+
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
+
 
 const NewsCard = ({ news }) => {
     const { _id, title, details, image_url, author, rating, total_view } = news;
@@ -33,20 +36,14 @@ const NewsCard = ({ news }) => {
 
                 </Card.Body>
                 <Card.Footer className="text-muted d-flex">
-                    <div className='flex-grow-1'>
-                        <Rating
-                             placeholderRating={rating.number}
-                             readonly
-                             emptySymbol={<FaRegStar></FaRegStar>}
-                             placeholderSymbol={<FaStar className='text-warning'></FaStar>}
-                             fullSymbol={<FaStar></FaStar>}
-                        
-                        
-                        > </Rating>
+                    <div className='flex-grow-1 d-flex align-items-center'>
+                    <Rating 
+                    style={{ maxWidth: 150 }}
+                    value= {Math.round(rating?.number || 0)} readOnly />
 
 
                        
-                        <span> {rating?.number} </span>
+                        <span className='ps-2'> {rating?.number} </span>
                     </div>
                     <div>
                         <FaEye></FaEye>{total_view}
